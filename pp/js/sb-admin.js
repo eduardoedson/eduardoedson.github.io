e10de65e0f362e034ba4ea5c00966a3128105ec2
Eduardo Edson Batista Cordeiro Alves <E006935@rede.tst>
2017-05-08 15:16:32 -0300

    Adiciona JS para tradução de DataTable

--- pp/js/sb-admin.js
+++ pp/js/sb-admin.js
@@ -8,35 +8,6 @@ $(document).scroll(function() {
     }
 });
 
-//https://codepad.remoteinterview.io/IFLBIJACIS
-// dataTables Example with BS4
-$(document).ready(function() {
-    $('#dataTable').DataTable({
-        "oLanguage": {
-            "sEmptyTable": "Nenhum registro encontrado",
-            "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
-            "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
-            "sInfoFiltered": "(Filtrados de _MAX_ registros)",
-            "sInfoPostFix": "",
-            "sInfoThousands": ".",
-            "sLengthMenu": "_MENU_ resultados por página",
-            "sLoadingRecords": "Carregando...",
-            "sProcessing": "Processando...",
-            "sZeroRecords": "Nenhum registro encontrado",
-            "sSearch": "Pesquisar",
-            "oPaginate": {
-                "sNext": "Próximo",
-                "sPrevious": "Anterior",
-                "sFirst": "Primeiro",
-                "sLast": "Último"
-            },
-            "oAria": {
-                "sSortAscending": ": Ordenar colunas de forma ascendente",
-                "sSortDescending": ": Ordenar colunas de forma descendente"
-            }
-        }
-    });
-});
 
 // -- Set new default font family and font color to mimic Bootstrap's default styling
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
