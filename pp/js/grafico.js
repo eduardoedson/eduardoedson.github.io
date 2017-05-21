7dd5cd9fd1d1ee78d3bea6ceb15399f552ed4d04
Eduardo Edson Batista Cordeiro Alves <edu-23@hotmail.com>
2017-05-21 15:57:53 -0300

    Adiciona gráfico de resumo

---
+++ pp/js/grafico.js
@@ -0,0 +1,49 @@
+var ctx = document.getElementById("grafico");
+var myLineChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio"],
+        datasets: [{
+            label: "Sessions",
+            lineTension: 0.3,
+            backgroundColor: "rgba(2,117,216,0.2)",
+            borderColor: "rgba(2,117,216,1)",
+            pointRadius: 5,
+            pointBackgroundColor: "rgba(2,117,216,1)",
+            pointBorderColor: "rgba(255,255,255,0.8)",
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(2,117,216,1)",
+            pointHitRadius: 20,
+            pointBorderWidth: 2,
+            data: [3, 1, 0, 1, 1],
+        }],
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                time: {
+                    unit: 'date'
+                },
+                gridLines: {
+                    display: false
+                },
+                ticks: {
+                    maxTicksLimit: 7
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    min: 0,
+                    max: 5,
+                    maxTicksLimit: 5
+                },
+                gridLines: {
+                    color: "rgba(0, 0, 0, .125)",
+                }
+            }],
+        },
+        legend: {
+            display: false
+        }
+    }
+});
