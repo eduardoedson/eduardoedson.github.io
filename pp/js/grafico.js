1e75ae35ce2938c4bc7775cf11bf682f42a3d2e6
Eduardo Edson Batista Cordeiro Alves <edu-23@hotmail.com>
2017-05-22 15:53:22 -0300

    adiciona mais informações do grafico de resumo

--- pp/js/grafico.js
+++ pp/js/grafico.js
@@ -2,20 +2,20 @@ var ctx = document.getElementById("grafico");
 var myLineChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio"],
+        labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
         datasets: [{
-            label: "Sessions",
+            label: "Consultas",
             lineTension: 0.3,
             backgroundColor: "rgba(2,117,216,0.2)",
             borderColor: "rgba(2,117,216,1)",
             pointRadius: 5,
-            pointBackgroundColor: "rgba(2,117,216,1)",
+            pointBackgroundColor: "rgba(255,99,47,1)",
             pointBorderColor: "rgba(255,255,255,0.8)",
             pointHoverRadius: 5,
             pointHoverBackgroundColor: "rgba(2,117,216,1)",
             pointHitRadius: 20,
             pointBorderWidth: 2,
-            data: [3, 1, 0, 2, 3],
+            data: [4, 5, 0, 5, 4, 1, 2, 1, 1, 5, 0, 2],
         }],
     },
     options: {
@@ -28,13 +28,13 @@ var myLineChart = new Chart(ctx, {
                     display: false
                 },
                 ticks: {
-                    maxTicksLimit: 7
+                    maxTicksLimit: 12
                 }
             }],
             yAxes: [{
                 ticks: {
                     min: 0,
-                    max: 5,
+                    max: 10,
                     maxTicksLimit: 5
                 },
                 gridLines: {
